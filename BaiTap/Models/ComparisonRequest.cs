1adfafb22d2292def01a20204281f86870bdd441
Nguyễn Đức Toán <157385254+Toan1234565@users.noreply.github.com>
2024-12-26 22:49:00 +0700

    Delete BaiTap directory

--- BaiTap/Models/ComparisonRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿using BaiTap.Models;
-using System.Collections.Generic;
-
-public class ComparisonRequest
-{
-    public List<int> Sanpham { get; set; }
-    public List<Criteria> Criteria { get; set; }
-}
